afe35e3257470d2e4e1e4c498b7f928991fe1b84
Depleted <odepleted@gmail.com>
2024-08-11 18:39:45 +1200

    chore: update project information

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,11 +17,12 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "LunaLabs", // Usually your GitHub org/user name.
-  projectName: "islestats", // Usually your repo name.
+  organizationName: "IsleStats", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
